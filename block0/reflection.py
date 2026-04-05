668c37786ec483f84e40f30f612b109a50e98664
Proony <72231617+Proony@users.noreply.github.com>
2026-04-05 15:08:27 -0700

    Added new line hehe

--- block0/reflection.py
+++ block0/reflection.py
@@ -1,3 +1,4 @@
 print("I installed Visual Code and learnt a little how to connect it to GitHub")
 print("I am also annoyed that AI helps to write my code")
 print("I need to practice more to get better at coding and not rely on AI")
+print("Hehe")
