69daecde7abeedd126b3a0aa77cb5bd70f61251a
Avoy Banik <avoy2002@gmail.com>
2026-04-05 15:04:19 -0700

    Added three reflections

---
+++ block0/reflection.py
@@ -0,0 +1,3 @@
+print("I installed Visual Code and learnt a little how to connect it to GitHub")
+print("I am also annoyed that AI helps to write my code")
+print("I need to practice more to get better at coding and not rely on AI")
